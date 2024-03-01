d53a6463e5a530db3876a3d85952d6c0d193be92
hyeseon <144360439+hyeseon97@users.noreply.github.com>
2024-03-01 20:26:20 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 200 ms, Memory: 20784 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -14,28 +14,32 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		int[] dist = new int[200001];
 		
+		// 시간이 빠른 순서로 재정렬
 		PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[1]-o2[1];
 			}
 		});
+		q.add(new int[] {N, 0});
 		
+		// 최단시간을 모두 최대값으로 초기화
 		Arrays.fill(dist, Integer.MAX_VALUE);
 		dist[N] = 0;
-		q.add(new int[] {N, 0});
 		
 		while(!q.isEmpty()) {
 			int[] temp = q.poll();
 			int x = temp[0];
 			int time = temp[1];
 			
-//			System.out.println(x + " " + time);
-			
+			// 다익스트라로 최단시간 구하기
+			// 동생에게 도착했을 때 종료하기
 			if(x==K) {
 				break;
 			}
 			
+			// -1로 이동할때는 현재 시간에서 1초경과한 시간이 -1 위치의 최단시간보다 짧을때 가기
+			// 0미만의 좌표로 이동하는것은 의미없으므로 0초과일때만 처리하기
 			if(x>0) {
 				if(time+1<dist[x-1]) {
 					dist[x-1] = time+1;
@@ -43,6 +47,8 @@ public class Main {
 				}
 			}
 			
+			// +1로 이동할때는 현재 시간에서 1초경과한 시간이 +1 위치의 최단시간보다 짧을때 가기
+			// 100000초과의 좌표로 이동하는것은 의미없으므로 100000미만일때만 처리하기
 			if(x<100000) {
 				if(time+1<dist[x+1]) {
 					dist[x+1] = time+1;
@@ -50,6 +56,9 @@ public class Main {
 				}
 			}
 			
+			// *2로 이동할때는 현재 시간으로 바로 비교하기 (*2는 0초가 걸리므로)
+			// *2 위치의 최단시간보다 짧을때 가기
+			// 100000 초과일때 *2로 이동하는것은 의미없으므로 100000이하일때만 처리하기
 			if(x<=100000) {
 				if(time<dist[x*2]) {
 					dist[x*2] = time;
