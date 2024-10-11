61a8776453ada4e00298c56441b20b8e97208f56
박혜선 (Hyeseon Park) <144360439+hyeseon97@users.noreply.github.com>
2024-10-11 23:47:36 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 작성해주세요
+SELECT E.EMP_NO, E.EMP_NAME,
+       (CASE WHEN SUM(G.SCORE)/2>=96 THEN 'S'
+             WHEN SUM(G.SCORE)/2>=90 THEN 'A'
+             WHEN SUM(G.SCORE)/2>=80 THEN 'B'
+             ELSE 'C'
+       END) AS GRADE,
+       (CASE WHEN SUM(G.SCORE)/2>=96 THEN E.SAL*0.2
+             WHEN SUM(G.SCORE)/2>=90 THEN E.SAL*0.15
+             WHEN SUM(G.SCORE)/2>=80 THEN E.SAL*0.1
+             ELSE 0
+       END) AS BONUS
+FROM HR_EMPLOYEES E
+INNER JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY E.EMP_NO
